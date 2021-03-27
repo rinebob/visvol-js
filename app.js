0a2a7d5f5164b4f227c78f0101de24371e65c19e
Bob Rinehart <rinebob111185@gmail.com>
2021-03-27 10:29:41 -0700

    Added chart input form controls and chart window html and css

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt', 'heyyy im writing to filesync dude!');
-fs.appendFileSync('notes.txt', 'is this really happening???');
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt', 'heyyy im writing to filesync dude!');
+// fs.appendFileSync('notes.txt', 'is this really happening???');
+
+const form = document.getElementById('tickerForm');
+console.log(form);
